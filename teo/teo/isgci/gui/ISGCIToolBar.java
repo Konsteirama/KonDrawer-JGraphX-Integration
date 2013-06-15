0a8e76316acffd6de6c1215f4d91ef23dbe10ddd
Sebastian Hubenschmid <SebastianHubenschmid@gmail.com>
2013-06-15 13:34:25 +0200

    Moved IconButtons to abstract factory

--- teo/teo/isgci/gui/ISGCIToolBar.java
+++ teo/teo/isgci/gui/ISGCIToolBar.java
@@ -11,15 +11,13 @@
 
 package teo.isgci.gui;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JToolBar;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 import teo.isgci.drawing.DrawingLibraryInterface;
 import teo.isgci.drawing.GraphManipulationInterface;
@@ -33,45 +31,6 @@ public class ISGCIToolBar extends JToolBar {
      * Change this every time this class is changed.
      */
     private static final long serialVersionUID = 1L;
-
-    /** The relative path to where all icons saved. */
-    private static final String ICONPATH = "/icons/";
-
-    /** Filename for UndoIcon. */
-    private static final String UNDO_ICON = "Undo.gif";
-
-    /** Filename for RedoIcon. */
-    private static final String REDO_ICON = "Redo.gif";
-
-    /** Filename for ExportIcon. */
-    private static final String EXPORT_ICON = "Export.gif";
-
-    /** Filename for ResetIcon. */
-    private static final String RESET_ICON = "Help.gif";
-
-    /** Filename for RestoreIcon. */
-    private static final String RESTORE_ICON = "Help.gif";
-
-    /** Filename for Zoom In Icon. */
-    private static final String ZOOM_IN_ICON = "ZoomIn.gif";
-
-    /** Filename for Zoom Out Icon. */
-    private static final String ZOOM_OUT_ICON = "ZoomOut.gif";
-
-    /** Filename for Zoom Icon. */
-    private static final String ZOOM_ICON = "Zoom.gif";
-
-    /** Filename for Zoom to Fit Icon. */
-    private static final String ZOOM_TO_FIT_ICON = "Help.gif";
-
-    /** Filename for Delete Icon. */
-    private static final String DELETE_ICON = "Remove.gif";
-
-    /** Filename for Search Icon. */
-    private static final String SEARCH_ICON = "Find.gif";
-
-    /** Filename for New Drawing Icon. */
-    private static final String NEW_DRAWING_ICON = "Add.gif";
 
     /** Reference to parent-ISGCI Mainframe for opening dialogs etc. */
     private ISGCIMainFrame mainframe;
@@ -140,8 +99,8 @@ public class ISGCIToolBar extends JToolBar {
     private void addGeneralButtons() {
         // NEW DRAWING
         String newDrawingTooltip = "Open a dialog to create a new drawing";
-        JButton newdrawingbutton 
-            = createImageButton(NEW_DRAWING_ICON, "Draw", newDrawingTooltip);
+        JButton newdrawingbutton  = IconButtonFactory.createImageButton(
+                IconButtonFactory.ADD_ICON, "Draw", newDrawingTooltip);
         add(newdrawingbutton);
 
         newdrawingbutton.addActionListener(new ActionListener() {
@@ -155,8 +114,8 @@ public class ISGCIToolBar extends JToolBar {
         
         // EXPORT
         String exportTooltip = "Open the export dialog";
-        JButton exportbutton 
-            = createImageButton(EXPORT_ICON, "Export", exportTooltip);
+        JButton exportbutton = IconButtonFactory.createImageButton(
+                IconButtonFactory.EXPORT_ICON, "Export", exportTooltip);
         add(exportbutton);
 
         exportbutton.addActionListener(new ActionListener() {
@@ -175,7 +134,8 @@ public class ISGCIToolBar extends JToolBar {
     private void addGraphControlButtons() {
         // UNDO
         String undoTooltip = "Undo the last action";
-        undoButton = createImageButton(UNDO_ICON, undoTooltip);
+        undoButton = IconButtonFactory.createImageButton(
+                IconButtonFactory.UNDO_ICON, undoTooltip);
         // undoButton.setEnabled(false);
         add(undoButton);
 
@@ -197,10 +157,53 @@ public class ISGCIToolBar extends JToolBar {
                 }
             }
         });
+        
+        // REAPPLYLAYOUT
+        String resetTooltip = "Reset the graph's layout";
+        JButton resetbutton = IconButtonFactory.createImageButton(
+                IconButtonFactory.REFRESH_ICON, resetTooltip);
 
+        add(resetbutton);
+        
+        resetbutton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GraphManipulationInterface<?, ?> graphManipulation = 
+                        getManipulationInterface();
+                
+                if (graphManipulation != null) {
+                    getManipulationInterface();
+                    graphManipulation.reapplyHierarchicalLayout();
+                    
+                }
+            }
+        });
+
+
+        // RESTORELAYOUT
+        String restoreTooltip = "Restore the graph to its original state";
+        JButton restorebutton = IconButtonFactory.createImageButton(
+                IconButtonFactory.HELP_ICON, restoreTooltip);
+        add(restorebutton);
+
+        restorebutton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GraphManipulationInterface<?, ?> graphManipulation = 
+                        getManipulationInterface();
+                
+                if (graphManipulation != null) {
+                    graphManipulation.resetLayout();
+                }
+            }
+        });
+        
         // REDO
         String redoTooltip = "Redo the last undo action";
-        redoButton = createImageButton(REDO_ICON, redoTooltip);
+        redoButton = IconButtonFactory.createImageButton(
+                IconButtonFactory.REDO_ICON, redoTooltip);
         // redoButton.setEnabled(false);
         add(redoButton);
 
@@ -222,45 +225,7 @@ public class ISGCIToolBar extends JToolBar {
                 }
             }
         });
-        
-        // REAPPLYLAYOUT
-        String resetTooltip = "Reset the graph's layout";
-        JButton resetbutton = createImageButton(RESET_ICON, resetTooltip);
-        add(resetbutton);
 
-        resetbutton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GraphManipulationInterface<?, ?> graphManipulation = 
-                        getManipulationInterface();
-                
-                if (graphManipulation != null) {
-                    getManipulationInterface();
-                    graphManipulation.reapplyHierarchicalLayout();
-                    
-                }
-            }
-        });
-
-        // RESTORELAYOUT
-        String restoreTooltip = "Restore the graph to its original state";
-        JButton restorebutton 
-            = createImageButton(RESTORE_ICON, restoreTooltip);
-        add(restorebutton);
-
-        restorebutton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                GraphManipulationInterface<?, ?> graphManipulation = 
-                        getManipulationInterface();
-                
-                if (graphManipulation != null) {
-                    graphManipulation.resetLayout();
-                }
-            }
-        });
     }
     
     /**
@@ -269,8 +234,8 @@ public class ISGCIToolBar extends JToolBar {
     private void addZoomControls() {
         // ZOOM IN
         String zoomInTooltip = "Zoom In";
-        JButton zoominbutton = createImageButton(ZOOM_IN_ICON, zoomInTooltip);
-        add(zoominbutton);
+        JButton zoominbutton = IconButtonFactory.createImageButton(
+                IconButtonFactory.ZOOM_IN_ICON, zoomInTooltip);
 
         zoominbutton.addActionListener(new ActionListener() {
 
@@ -291,6 +256,11 @@ public class ISGCIToolBar extends JToolBar {
         final JComboBox zoomBox = new JComboBox(new String[] { defaultEntry, 
                 "100%", "200%", "300%", zoomToFit });
         add(zoomBox);
+        
+        // make sure zoombox doesn't get bigger
+        final Dimension zoomBoxSize = new Dimension(100, 50);
+        zoomBox.setMaximumSize(zoomBoxSize);
+        
         
         zoomBox.addActionListener(new ActionListener() {
             
@@ -324,8 +294,8 @@ public class ISGCIToolBar extends JToolBar {
         
         // ZOOM OUT
         String zoomOutTooltip = "Zoom Out";
-        JButton zoomoutbutton = createImageButton(ZOOM_OUT_ICON,
-                zoomOutTooltip);
+        JButton zoomoutbutton  = IconButtonFactory.createImageButton(
+                IconButtonFactory.ZOOM_OUT_ICON, zoomOutTooltip);
         add(zoomoutbutton);
 
         zoomoutbutton.addActionListener(new ActionListener() {
@@ -340,35 +310,6 @@ public class ISGCIToolBar extends JToolBar {
                 }
             }
         });
-
-
-//        String zoomTooltip = "?";
-//        JButton zoombutton = createImageButton(ZOOM_ICON, zoomTooltip);
-
-
-//        zoombutton.addActionListener(new ActionListener() {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                getManipulationInterface();
-//                // TODO
-//            }
-//        });
-
-        // ZOOM TO FIT
-//        String zoomToFitTooltip = "Zoom to fit everything into the canvas";
-//        JButton zoomtofitbutton = createImageButton(ZOOM_TO_FIT_ICON, 
-//                "Zoom to fit", zoomToFitTooltip);
-//        add(zoomtofitbutton);
-//
-//        zoomtofitbutton.addActionListener(new ActionListener() {
-//
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                getManipulationInterface();
-//                // TODO
-//            }
-//        });
     }
     
     /**
@@ -377,8 +318,8 @@ public class ISGCIToolBar extends JToolBar {
     private void addMiscButtons() {
         // DELETE
         String deleteTooltip = "?";
-        JButton deletebutton 
-            = createImageButton(DELETE_ICON, deleteTooltip);
+        JButton deletebutton = IconButtonFactory.createImageButton(
+                IconButtonFactory.DELETE_ICON, deleteTooltip);
         add(deletebutton);
 
         deletebutton.addActionListener(new ActionListener() {
@@ -391,9 +332,10 @@ public class ISGCIToolBar extends JToolBar {
         });
 
         // SEARCH
-        String searchTooltip = "Searches for the specific node in the canvas.";
-        JButton searchbutton 
-            = createImageButton(SEARCH_ICON, searchTooltip);
+        String searchTooltip = "Opens a dialog to search for a specific "
+                                + "graphclass in the drawing.";
+        JButton searchbutton = IconButtonFactory.createImageButton(
+                IconButtonFactory.SEARCH_ICON, searchTooltip);
         add(searchbutton);
 
         searchbutton.addActionListener(new ActionListener() {
@@ -405,65 +347,6 @@ public class ISGCIToolBar extends JToolBar {
         });
     }
     
-    /**
-     * Creates a new button with specified icon as image.
-     * 
-     * @param icon
-     *          Relative path to icon.
-     * @param description
-     *            The description of the ImageIcon and the tooltip text for the
-     *            button.
-     * @return
-     *  A new JButton created from icon and tooltip or a button with 
-     *          ? if the path was invalid
-     */
-    private JButton createImageButton(String icon, String description) {
-        return createImageButton(icon, "", description);
-    }
-    
-    /**
-     * Returns a new Button with imageicon, or a button with ? if the path
-     * was invalid.
-     * 
-     * @param icon
-     *            The relative path to {@link #ICONPATH} to the icon
-     *            
-     * @param name
-     *            A name that will be displayed next to the icon. Can be null.
-     *            
-     * @param description
-     *            The description of the ImageIcon and the tooltip text for the
-     *            button.
-     * 
-     * @return A new JButton created from icon, name and tooltip or a button 
-               with ? if the path was invalid
-     */
-    private JButton createImageButton(String icon,
-                                      String name,
-                                      String description) {
-        String path = ICONPATH + icon;
-        JButton button;
-
-        java.net.URL imgURL = getClass().getResource(path);
-        
-        if (imgURL != null) {
-            
-            ImageIcon imgIcon = new ImageIcon(imgURL, description);
-            button = new JButton(name, imgIcon);
-            
-        } else {
-            
-            System.err.println("Couldn't find file: " + path);
-            if (name.isEmpty()) {
-                button = new JButton("?");
-            } else {
-                button = new JButton(name);
-            }
-        }
-
-        button.setToolTipText(description);
-        return button;
-    }
 }
 
 /* EOF */
